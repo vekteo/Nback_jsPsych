384282ea0d88907fe80241d7c2083122ce2dd38c
Teodóra Vékony <45353438+vekteo@users.noreply.github.com>
2021-03-03 14:51:01 +0100

    create parameters.js
    Move here the parameters which can be set by the user

---
+++ parameters.js
@@ -0,0 +1,6 @@
+const level = 0; //available levels: 0, 1, 2
+const trialDuration = 1500;
+const stimulusDuration = 500;
+const feedBackDuration = 1000;
+
+const language = en; //available languages: english (en), hungarian (hu)
