8b3c58e1730844f6cf725f331ccfa94725744fdb
Teodóra Vékony <45353438+vekteo@users.noreply.github.com>
2021-03-25 14:58:50 +0100

    change comment about available languages

--- parameters.js
+++ parameters.js
@@ -3,4 +3,4 @@ const fixationDuration = 500;
 const letterDuration = 1500;
 const feedBackDuration = 1000;
 
-const language = en; //available languages: english (en), hungarian (hu)
+const language = pt; //available languages: english (en), hungarian (hu), french (fr), spanish (es) and portuguese (pt)
